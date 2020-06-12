f36937ee9a9ce31d5105d8e6890a97be66820269
Faraz Khan <you@example.com>
2020-06-12 20:14:00 +0500

    main page php added

---
+++ main.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Retail Property</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</head>
+<body>
+  <div style="background-color: rgb(240, 238, 238); padding-bottom: 10%;"> 
+<?php
+require_once('navbar.html');
+?>
+
+<!--row 1-->
+    <div class="container" style= "text-align: center; margin-top: 6%;">
+    <h2 style="font-family: Arial Nova Light;">Shop Today's Trending Deals and Save Big</h2>
+    <div class="card-deck" style="padding-top: 3%;">
+        <div class="card shadow" style="padding: 2%;">
+          <img class="card-img-top" src="pic1.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Offer 1</h5>
+            <p class="card-text" style="text-align: justify">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">More Details</small>
+          </div>
+        </div>
+        <div class="card shadow" style="padding: 2%;">
+          <img class="card-img-top" src="pic2.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Offer 2</h5>
+            <p class="card-text" style="text-align:justify">This card has supporting text below as a natural lead-in to additional content.</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">More Details</small>
+          </div>
+        </div>
+        <div class="card shadow" style="padding: 2%;">
+          <img class="card-img-top" src="pic3.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Offer 3</h5>
+            <p class="card-text" style="text-align:justify">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">More Details</small>
+          </div>
+        </div>
+      </div>
+</div>
+
+<!--row 2-->
+<div class="container" style= "text-align: center; margin-top: 6%;">
+    <h2 style="font-family: Arial Nova Light;">TRENDING</h2>
+    <div class="card-deck" style="padding-top: 3%;">
+        <div class="card shadow" style="padding: 2%;">
+          <img class="card-img-top" src="pic1.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Trend 1</h5>
+            <p class="card-text" style="text-align:justify">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">More Details</small>
+          </div>
+        </div>
+        <div class="card shadow" style="padding: 2%;">
+          <img class="card-img-top" src="pic2.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Trend 2</h5>
+            <p class="card-text" style="text-align:justify">This card has supporting text below as a natural lead-in to additional content.</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">More Details</small>
+          </div>
+        </div>
+        <div class="card shadow" style="padding: 2%;">
+          <img class="card-img-top" src="pic3.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Trend 3</h5>
+            <p class="card-text" style="text-align:justify">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">More Details</small>
+          </div>
+        </div>
+      </div>
+</div>
+</div>
+
+
+<!--main div ends-->
+</div>
+</body>
+</html>
