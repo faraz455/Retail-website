832cb1d4a5dbec091fd8604375ea827003e61264
Faraz Khan <you@example.com>
2020-06-12 19:56:36 +0500

    login html page turned to php

---
+++ login.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Retail Property</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="main.css">
+
+</head>
+<body>
+  <div style="background-color: rgb(240, 238, 238); padding-bottom: 10%;"> 
+  <?php
+require_once('navbar.html');
+  ?>
+ 
+<!--///////////////////////////////////////////////////////////////////////////////////////////////////////////-->
+<!--<div class="container">
+    <div class="page-header">
+        <h1>Welcome back!</h1>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="panel with-nav-tabs panel-info">
+                <div class="panel-heading">
+                        <ul class="nav nav-tabs">
+                            <li class="active"><a href="#tab1info" data-toggle="tab">User</a></li>
+                            <li><a href="#tab2info" data-toggle="tab">Admin</a></li>
+                        </ul>
+                </div>
+                <div class="panel-body">
+                    <div class="tab-content">
+                        <div class="tab-pane fade in active" id="tab1info">Info 1</div>
+                        <div class="tab-pane fade" id="tab2info">Info 2</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+	</div>
+</div>--s>
+
+
+<div>
+
+</div>-->
+
+    <!--main div ends-->
+</div>
+
+</body>
+</html>
