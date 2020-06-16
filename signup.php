b01c71d06efaade74854484c3202e785f1ab4b4e
Faraz Khan <you@example.com>
2020-06-17 00:56:05 +0500

    sign up page back end  added

--- signup.php
+++ signup.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+//including the database connection file
+include_once("connect.php");
+?>
 <!DOCTYPE html>
 <html>
 
@@ -30,13 +35,14 @@ require_once('navbar.html');
     
    </div>
 <br>
-
+<form action="signup.php" method="post" name="form1">
         <div class="row size">
             <div class="input-group mb-3" style=" padding-left: 7%;">
                 <div class="input-group-prepend">
                   <span class="input-group-text" id="basic-addon1"><i class="fa fa-user icon"></i></span>
                 </div>
-                <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+                
+                <input type="text" class="form-control" placeholder="Username" name = "user_name" aria-label="Username" aria-describedby="basic-addon1">
             </div>
         </div>
 
@@ -45,7 +51,7 @@ require_once('navbar.html');
                 <div class="input-group-prepend">
                     <span class="input-group-text" id="basic-addon1"> <i class="fa fa-envelope" ></i></span>
                 </div>
-                <input type="email" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1">
+                <input type="email" class="form-control" placeholder="Email" name = "email" aria-label="Email" aria-describedby="basic-addon1">
             </div>
         </div>
 
@@ -54,7 +60,7 @@ require_once('navbar.html');
                 <div class="input-group-prepend">
                     <span class="input-group-text" id="basic-addon1"><i class="fa fa-key icon"></i></span>
                 </div>
-                <input type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1">
+                <input type="password" class="form-control" placeholder="Password" name = "password" aria-label="Password" aria-describedby="basic-addon1">
             </div>
         </div>
 
@@ -63,7 +69,7 @@ require_once('navbar.html');
                 <div class="input-group-prepend">
                     <span class="input-group-text" id="basic-addon1"><i class="fa fa-key icon"></i></span>
                 </div>
-                <input type="password" class="form-control" placeholder="Confirm Password" aria-label="Password" aria-describedby="basic-addon1">
+                <input type="password" class="form-control" placeholder="Confirm Password" name = "c_password" aria-label="Password" aria-describedby="basic-addon1">
             </div>
         </div>
 
@@ -214,11 +220,56 @@ require_once('navbar.html');
         </div>
     </div>
 </div>
-
-<div class="row size2" style=" padding-left: 40%;">
-        <button type="button" class="btn btn-success size2" style="width: 100px;"  >SignUp</button>
-        <br>
 </div>
+<div class="row size">
+    <div class="input-group mb-3" style=" padding-left: 10%;">
+   
+    <button type="submit" name = "signup" class="btn btn-success size btn-lg btn-block"  >Sign Up</button>
+    </form>
+    <br>
+    </div>
+</div>
+</div>
+<?php
+$result = mysqli_query($mysqli, "SELECT * FROM user_info "); 
+
+if(isset($_POST['signup'])) {	
+	$user_name = mysqli_real_escape_string($mysqli, $_POST['user_name']);
+    $password = mysqli_real_escape_string($mysqli, $_POST['password']);
+    $c_password = mysqli_real_escape_string($mysqli, $_POST['c_password']);
+	$email = mysqli_real_escape_string($mysqli, $_POST['email']);
+    
+    // checking emoty field
+    $flag1 = 0; 
+
+    //checking if user exist
+    $flag2 = 0;
+
+    //checking if the password and confirm password matches
+    $flag3 = 0;
+
+    if($user_name == "" || $password == "" || $c_password == "" || $email == "") 
+        { // empty field
+            $flag1 = 1;   
+        }
+    else{
+        while($res = mysqli_fetch_array($result)) { 
+            //user exist
+            if($res['user_name'] == $user_name || $res['email'] == $email ){
+                $flag2=1;
+        }
+    if($password != $c_password){
+        $flag3 =1;
+    }
+
+    // all things satisfied profile signed up
+    if ($flag1==0 && $flag2 ==0 && $flag3==0){
+        $result = mysqli_query($mysqli, "INSERT INTO `user_info` ( `user_name`, `email`, `password`) VALUES ('$user_name','$email','$password');");
+    }
+ $mysqli->close();
+}
+?>
+
 
 <!--main div ends-->
 </div>
