cb03ff00b74195b030feb1ea05408d16b69f76db
Faraz Khan <you@example.com>
2020-06-26 03:17:54 +0500

    session variable name changed

--- html pages/connect.php
+++ html pages/connect.php
@@ -7,8 +7,8 @@ $databasePassword = '';
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
 
 // initialize loggedIn if it doesn't already exist
-if (!isset($_SESSION['loggedIn']))
-    {$_SESSION['loggedIn'] = 0;}
+if (!isset($_SESSION['logIn']))
+    {$_SESSION['logIn'] = 0;}
 if(!isset($_SESSION['login_user'])){
     $_SESSION['login_user'] = "";
 }
