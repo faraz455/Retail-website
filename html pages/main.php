dd7a1e1439d575134159a1d374db8721ab26f0a6
Faraz Khan <you@example.com>
2020-07-10 00:48:54 +0500

    display image using database

--- html pages/main.php
+++ html pages/main.php
@@ -7,6 +7,36 @@
 <body>
 <?php
 require_once('navbar.php');
+
+$result = mysqli_query($mysqli, "SELECT * FROM trending_section"); 
+
+$price = array("1","1","1");
+$location = array(" "," "," ");
+$texts = array(" "," "," ");
+
+
+      for($x = 0; $res = mysqli_fetch_array($result) and $x < 3; $x++){  
+            $price[$x]= $res['item_price'];
+            $location[$x] = $res['item_loc'];
+            $texts[$x]= $res['item_text'];
+
+      }
+
+
+$result = mysqli_query($mysqli, "SELECT * FROM new_section"); 
+
+$n_price = array("1","1","1");
+$n_location = array(" "," "," ");
+$n_texts = array(" "," "," ");
+
+
+      for($x = 0; $res = mysqli_fetch_array($result) and $x < 3; $x++){  
+            $n_price[$x]= $res['item_price'];
+            $n_location[$x] = $res['item_loc'];
+            $n_texts[$x]= $res['item_text'];
+
+      }
+
 ?>
 
 <!--row 1-->
@@ -14,33 +44,47 @@ require_once('navbar.php');
     <h2 style="font-family: Arial Nova Light;">Shop Today's Trending Deals and Save Big</h2>
     <div class="card-deck" style="padding-top: 3%;">
         <div class="card shadow" style="padding: 2%;">
-          <img class="card-img-top" src="../Images/pic1.jpg" alt="Card image cap">
+
+     <?php    echo '<img class="card-img-top" src="'.$location[0].'" alt="Card image cap">' ;  ?>
+
           <div class="card-body">
             <h5 class="card-title">Offer 1</h5>
-            <p class="card-text" style="text-align: justify">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+     <?php echo ' <p class="card-text" style="text-align: justify">'.$texts[0].'</p>'; ?>
           </div>
           <div class="card-footer">
-            <small class="text-muted">More Details</small>
+          <?php echo '<small class="text-muted">Rs'.$price[0].'</small>' ?>
           </div>
         </div>
         <div class="card shadow" style="padding: 2%;">
-          <img class="card-img-top" src="../Images/pic2.jpg" alt="Card image cap">
+
+        <?php  echo '<img class="card-img-top" src="'.$location[1].'" alt="Card image cap">' ;  ?>
+
           <div class="card-body">
             <h5 class="card-title">Offer 2</h5>
-            <p class="card-text" style="text-align:justify">This card has supporting text below as a natural lead-in to additional content.</p>
+
+            <?php echo ' <p class="card-text" style="text-align: justify">'.$texts[1].'</p>'; ?>
+  
           </div>
           <div class="card-footer">
-            <small class="text-muted">More Details</small>
+
+          <?php echo '<small class="text-muted">Rs'.$price[1].'</small>' ?>
+            
           </div>
         </div>
         <div class="card shadow" style="padding: 2%;">
-          <img class="card-img-top" src="../Images/pic3.jpg" alt="Card image cap">
+
+        <?php    echo '<img class="card-img-top" src="'.$location[2].'" alt="Card image cap">' ;  ?>
+
           <div class="card-body">
             <h5 class="card-title">Offer 3</h5>
-            <p class="card-text" style="text-align:justify">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+
+            <?php echo ' <p class="card-text" style="text-align: justify">'.$texts[2].'</p>'; ?>
+            
           </div>
           <div class="card-footer">
-            <small class="text-muted">More Details</small>
+
+          <?php echo '<small class="text-muted">Rs'.$price[2].'</small>' ?>
+            
           </div>
         </div>
       </div>
@@ -48,40 +92,55 @@ require_once('navbar.php');
 
 <!--row 2-->
 <div class="container" style= "text-align: center; margin-top: 6%;">
-    <h2 style="font-family: Arial Nova Light;">TRENDING</h2>
-    <div class="card-deck" style="padding-top: 3%;">
-        <div class="card shadow" style="padding: 2%;">
-          <img class="card-img-top" src="../Images/pic1.jpg" alt="Card image cap">
-          <div class="card-body">
-            <h5 class="card-title">Trend 1</h5>
-            <p class="card-text" style="text-align:justify">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          </div>
-          <div class="card-footer">
-            <small class="text-muted">More Details</small>
-          </div>
+  <h2 style="font-family: Arial Nova Light;"> Trending Deals</h2>
+  <div class="card-deck" style="padding-top: 3%;">
+      <div class="card shadow" style="padding: 2%;">
+
+   <?php    echo '<img class="card-img-top" src="'.$n_location[0].'" alt="Card image cap">' ;  ?>
+
+        <div class="card-body">
+          <h5 class="card-title">Trend 1</h5>
+   <?php echo ' <p class="card-text" style="text-align: justify">'.$n_texts[0].'</p>'; ?>
         </div>
-        <div class="card shadow" style="padding: 2%;">
-          <img class="card-img-top" src="../Images/pic2.jpg" alt="Card image cap">
-          <div class="card-body">
-            <h5 class="card-title">Trend 2</h5>
-            <p class="card-text" style="text-align:justify">This card has supporting text below as a natural lead-in to additional content.</p>
-          </div>
-          <div class="card-footer">
-            <small class="text-muted">More Details</small>
-          </div>
-        </div>
-        <div class="card shadow" style="padding: 2%;">
-          <img class="card-img-top" src="../Images/pic3.jpg" alt="Card image cap">
-          <div class="card-body">
-            <h5 class="card-title">Trend 3</h5>
-            <p class="card-text" style="text-align:justify">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-          </div>
-          <div class="card-footer">
-            <small class="text-muted">More Details</small>
-          </div>
+        <div class="card-footer">
+        <?php echo '<small class="text-muted">Rs'.$n_price[0].'</small>' ?>
         </div>
       </div>
+      <div class="card shadow" style="padding: 2%;">
+
+      <?php  echo '<img class="card-img-top" src="'.$n_location[1].'" alt="Card image cap">' ;  ?>
+
+        <div class="card-body">
+          <h5 class="card-title">Trend 2</h5>
+
+          <?php echo ' <p class="card-text" style="text-align: justify">'.$n_texts[1].'</p>'; ?>
+
+        </div>
+        <div class="card-footer">
+
+        <?php echo '<small class="text-muted">Rs'.$n_price[1].'</small>' ?>
+          
+        </div>
+      </div>
+      <div class="card shadow" style="padding: 2%;">
+
+      <?php    echo '<img class="card-img-top" src="'.$n_location[2].'" alt="Card image cap">' ;  ?>
+
+        <div class="card-body">
+          <h5 class="card-title">Trend 3</h5>
+
+          <?php echo ' <p class="card-text" style="text-align: justify">'.$n_texts[2].'</p>'; ?>
+          
+        </div>
+        <div class="card-footer">
+
+        <?php echo '<small class="text-muted">Rs'.$n_price[2].'</small>' ?>
+          
+        </div>
+      </div>
+    </div>
 </div>
+
 </div>
 
 
